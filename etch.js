f1e4ec3d92e469824899ececc683bbe5efded4cc
Milo <142271649+milooo03@users.noreply.github.com>
2025-02-07 12:12:11 -0500

    Optimize code with functions, fix some logic, and remove unnecessary lines.
    Avoided repetition and achieved cleaner code by updating the logic of some lines, variables, and using functions.

--- etch.js
+++ etch.js
@@ -1,54 +1,46 @@
+//Initial grid creation
 let container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
-    let grid = document.createElement("div");
-    grid.classList.add("square");
-    container.appendChild(grid);
-};
-
-let p = document.createElement("p");
-container.appendChild(p);
-
-let grids = container.querySelectorAll(".square");
-
-grids.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "grey";
-});
-});
-
-const resizeButton = document.querySelector("button");
-let resizeValue;
-let newSize;
-
-resizeButton.addEventListener("click", () => {
-    resizeValue = +prompt("Enter the number of squares per side for the new grid (1-100).");
+function gridSize(size) {
+    let newSize = 480 / size;
     
-    if (resizeValue === null || isNaN(resizeValue) || resizeValue < 1 || resizeValue > 100 || resizeValue === "" || !Number.isInteger(resizeValue)) {
-        alert("Please enter a valid value.");
-        return;
-    }
-    
-    newSize = 480 / resizeValue;
-
-    grids.forEach((item) => {
-        item.remove();
-    });
-
-    for (let i = 0; i < (resizeValue * resizeValue); i++) {
+    for (let i = 0; i < (size * size); i++) {
         let grid = document.createElement("div");
         grid.classList.add("square");
         grid.style.width = `${newSize}px`;
         grid.style.height = `${newSize}px`;
         container.appendChild(grid);
     };
+}
 
-    grids = container.querySelectorAll(".square");
+//Select all grids and add color changing properties to them
+function changeGridColor() {
+    let grids = container.querySelectorAll(".square");
 
     grids.forEach((item) => {
         item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = "grey";
+            item.style.backgroundColor = "grey";
     });
     });
+}
+
+gridSize(16);
+changeGridColor();
+
+//Allow grid resizing through a button
+const resizeButton = document.querySelector("button");
+
+resizeButton.addEventListener("click", () => {
+    let resizeValue = +prompt("Enter the number of squares per side for the new grid (1-100).");
+    
+    if (resizeValue === null || isNaN(resizeValue) || resizeValue < 1 || resizeValue > 100 || resizeValue === "" || !Number.isInteger(resizeValue)) {
+        alert("Please enter a valid value.");
+        return;
+    }
+
+    container.innerHTML = "";
+
+    gridSize(resizeValue);
+    changeGridColor();
 });
 
